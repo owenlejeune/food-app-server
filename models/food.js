56b007d62bb858e41a04164805bc2a369671205a
Owen LeJeune <omlejeune@gmail.com>
2020-04-22 15:56:45 -0400

    Add Additional Food and Package Types

--- models/food.js
+++ models/food.js
@@ -4,13 +4,13 @@ let foodSchema = mongoose.Schema({
     description: String,
     foodType: {
         type: String,
-        enum: ['fruit', 'vegetable', 'dairy', 'sauce', 'grain', 'meat', 'snack', 'sweet', 'spice'],
+        enum: ['fruit', 'vegetable', 'dairy', 'sauce', 'grain', 'meat', 'snack', 'sweet', 'spice', 'bread', 'meal', 'soup', 'drink', 'spead', 'protein', 'starch', 'other'],
         default: 'other'
     },
     packageType: {
         type: String,
-        enum: ['piece', 'box', 'bag', 'container', 'bottle'],
-        default: 'piece'
+        enum: ['piece', 'box', 'bag', 'container', 'bottle', 'pack', 'can', 'carton', 'other'],
+        default: 'other'
     },
     quantity: Number
 }, { collection: 'food' });
