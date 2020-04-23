bb51250eeac632c0f4ac8304266fb08d8abdf0b1
Owen LeJeune <omlejeune@gmail.com>
2020-04-23 16:33:04 -0400

    update scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
+echo "Stopping server..."
 sudo systemctl stop food-server
+sudo systemctl --no-pager status food-server
+echo "Restarting server..."
 sudo systemctl enable food-server
 sudo systemctl start food-server
-sudo systemctl status food-server
+sudo systemctl --no-pager status food-server
